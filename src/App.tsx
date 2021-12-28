9f0dff400194c04088625f438f6adbe48fd9fac9
William Brito <williamrldb@gmail.com>
2021-12-28 15:25:12 -0400

    --Firebase and Material-ui configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
+import "./config/firebase";
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { SnackbarProvider } from 'notistack';
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical:'top',
+          horizontal:'right'
+        }}
+      >
+        <Home />
+      </SnackbarProvider>
+    </React.Fragment>
   );
 }
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  serverTimestamp,
+  collection,
+  query,
+  onSnapshot
+} from 'firebase/firestore';
+import { Box, Button, Container, TextField, Typography } from '@material-ui/core';
+import { v4 } from 'uuid';
+import { useSnackbar } from 'notistack';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+}
+
+const Home: React.FC = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [users, setUsers] = useState<User[]>([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleAdd = async () => {
+    try {
+      const db = getFirestore();
+
+      const id = v4();
+      const data = {
+        name,
+        email,
+        createdAt: serverTimestamp(),
+      }
+
+      await setDoc(doc(db, "users", id), data);
+
+      enqueueSnackbar('User added', { variant: 'success' });
+
+      setName('');
+      setEmail('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const db = getFirestore();
+    const q = query(collection(db, "users"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        };
+      }) as User[];
+
+      setUsers(data);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Container>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+        }}
+      >
+        <Typography variant='h2' component="h1">User</Typography>
+
+        <TextField
+          name="name"
+          label="Name"
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          autoComplete='off'
+        />
+
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete='off'
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleAdd}>
+          Add
+        </Button>
+      </Box>
+
+      <Box
+        style={{
+          marginTop: '3rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+        }}
+      >
+        {
+          users.map((user) => (
+            <Box key={user.id}>
+              <Typography variant='h3' component="h2">{user.name}</Typography>
+              <Typography variant='h4' component="h2">{user.email}</Typography>
+            </Box>
+          ))
+        }
+      </Box>
+    </Container>
+  );
+}
+
+export default Home;
