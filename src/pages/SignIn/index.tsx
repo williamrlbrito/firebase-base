8a5c195a5e2cf908e746d83eb3f9f1e651889efc
William Brito <williamrldb@gmail.com>
2021-12-29 09:12:34 -0400

    --Stylized login screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,29 +1,133 @@
 import React from 'react';
-import { Box, Button, Container } from '@material-ui/core';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
 import { useAuth } from '../../hooks/auth';
 
 const SignIn: React.FC = () => {
   const { signIn } = useAuth();
 
-  return (
-    <Container>
-      <Box>
-        <h1>Login</h1>
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() =>
-            signIn({
-              email: 'williamrldb@gmail.com',
-              password: '123456',
-            })
-          }
+  const handleSignIn = () => {
+    signIn({ email, password });
+  };
+
+  return (
+    <Box
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        style={{
+          height: '500px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          style={{
+            width: '480px',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '0 4rem',
+            borderRight: '1px solid #000',
+          }}
         >
-          Acessar
-        </Button>
+          <Typography variant="h4" component="h1">
+            Go Finance
+          </Typography>
+        </Box>
+
+        <Box
+          style={{
+            width: '480px',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            gap: '4rem',
+            padding: '0 4rem',
+            borderLeft: '1px solid #000',
+          }}
+        >
+          <Box
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '0.5rem',
+            }}
+          >
+            <Typography variant="h4" component="h2">
+              Welcome
+            </Typography>
+            <Typography variant="h6" component="p">
+              Please login to admin dashboard
+            </Typography>
+          </Box>
+          <Box
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1rem',
+            }}
+          >
+            <TextField
+              name="email"
+              label="E-mail"
+              variant="outlined"
+              fullWidth
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <TextField
+              name="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+            />
+          </Box>
+          <Box
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '0.5rem',
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              fullWidth
+              onClick={handleSignIn}
+            >
+              login
+            </Button>
+            <Typography variant="h6" component="p">
+              Forgotten your password?
+            </Typography>
+          </Box>
+        </Box>
       </Box>
-    </Container>
+    </Box>
   );
 };
 
